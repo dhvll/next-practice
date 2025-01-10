09a29f54cf962cafa67b11fcd3400985398ff1c6
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2025-01-10 12:07:28 +0530

    feat: establish database connection in sign-up route

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -4,6 +4,8 @@ import bcrypt from "bcryptjs"
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail"
 
 export async function POST(request: Request) {
+  await dbConnect()
+
   try {
     const { username, email, password } = await request.json()
     const existingVerifiedUserByUsername = await UserModel.findOne({
